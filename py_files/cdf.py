e3a61ae52bac003cd38f8390eeade6236cefd602
Georgia Ray <georgiaray_ic@Mac.communityfibre.co.uk>
2025-09-09 17:50:51 +0100

    small changes to cdf func

--- py_files/cdf.py
+++ py_files/cdf.py
@@ -2,28 +2,36 @@
 '''This file contains the functions to build the CDF and the inverse CDF of a given pdf. It also contains the functions to sample from the pdf and the inverse CDF.'''
 
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import PchipInterpolator
 import matplotlib.pyplot as plt
 
-def cdf_and_q(pdf, xs):
+def cdf_and_q(pdf, xs, rng = None):
+    rng = np.random.default_rng(rng)
     # build CDF values
     cdf_vals = np.cumsum((pdf[:-1] + pdf[1:]) * np.diff(xs) / 2.0) #note that the pdf and xs we are using are from the original pdf as defined just above this cell 
     #performing the trapezoidal integration per interval  
     cdf_vals = np.concatenate([[0.0], cdf_vals]) #accumulates those values from the starting point (in this case, it's not possible to have a true negative infinity value so that's why we are anchoring the CDF at 0 at the leftmost point of xs, effectively treating xs[0] as our numerical -∞; this is okay from a probability perspective because we are not integrating over the entire real line, we are only integrating over the support of the pdf)
+    cdf_vals /= cdf_vals[-1]
 
-    F = interp1d(xs, cdf_vals, bounds_error=False, fill_value=(0.0, 1.0))
+    # enforce strict monotonicity for inversion
+    cdf_vals = np.maximum.accumulate(cdf_vals)
+    eps = 1e-15
+    cdf_vals = np.clip(cdf_vals + eps*np.arange(len(cdf_vals)), 0.0, 1.0)
+
+    F = PchipInterpolator(xs, cdf_vals, extrapolate=True)
     #here we are just making this a continuous function that can be evaluated at any point in the domain of xs by interpolating the values of cdf_vals at the points of xsß
 
     # inverse CDF (quantile function)
-    Q = interp1d(cdf_vals, xs, bounds_error=False, fill_value=(xs[0], xs[-1]))
-    #here we are making the inverse of the CDF, so that we can map from the uniform distribution to the original pdf
+    Q = PchipInterpolator(cdf_vals, xs, extrapolate=True)
+    #here we are making the inverse of the CDF, so that we can map from the uniform distribution to the original pdfß
 
     # generate samples
-    u_samples = np.random.rand(1000000)    # uniform(0,1)
-    x_samples = Q(u_samples)            # map to your PDF
+    u_samples = rng.random(1000000)
+    u_samples = np.clip(u_samples, 1e-12, 1 - 1e-12)           # avoid edges
+    x_samples = Q(u_samples)           
 
     # apply CDF transform back to uniform
-    u_check = F(x_samples)              # should be ~ uniform
+    u_check = F(x_samples)
     return u_samples, x_samples, u_check
 
 def plot_cdf_transform(u_samples, x_samples, u_check, xs, pdf):
